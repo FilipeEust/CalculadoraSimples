159cf3ea8659547d806c322521312c0f7b21644c
FilipeEust <filiperafael007@gmail.com>
2024-11-03 20:54:03 -0300

    Update /src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,41 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-        System.out.print("Digite seu nome: ");
-        String nome = input.nextLine();
-        System.out.print("\n"+nome);
+
+        System.out.println("Primeiro valor: ");
+        Double valor1 =input.nextDouble();
+
+        System.out.println("Segundo valor: ");
+        Double valor2 =input.nextDouble();
+
+        System.out.println("Operador (+)(-)(/)(*): ");
+        String operacao = input.next();
+
+        input.close();
+
+        switch(operacao){
+            case "+":
+                System.out.println("Resultado: "+(valor1+valor2));
+                break;
+            case "-":
+                System.out.println("Resultado: "+(valor1-valor2));
+                break;
+            case "/":
+                System.out.println("Resultado: "+(valor1/valor2));
+                break;
+            case "*":
+                System.out.println("Resultado: "+(valor1*valor2));
+                break;
+            default:
+                System.out.println("ERRO: Operador invalido.");
+
+
+
+
+        }
     }
 }
